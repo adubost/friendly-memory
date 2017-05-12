0a6c039a5f42b27635d88132f45ca61e5784fdbf
adubost <adubost@arden.ac.uk>
2017-05-12 10:47:39 +0100

    Fix the issue for login connection

--- src/AD/AdminBundle/Controller/WelcomeController.php
+++ src/AD/AdminBundle/Controller/WelcomeController.php
@@ -4,6 +4,7 @@ namespace AD\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class WelcomeController extends Controller {
 
--- src/AD/UserBundle/DataFixtures/ORM/LoadUser.php
+++ src/AD/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -7,27 +7,47 @@ namespace AD\UserBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use AD\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadUser implements FixtureInterface {
+class LoadUser implements FixtureInterface, ContainerAwareInterface {
+
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
 
     public function load(ObjectManager $manager) {
+
 
         $listnames = array('christine', 'aude', 'nick', 'chris', 'matt', 'samir', 'dan');
         foreach ($listnames as $name) {
             $user = new User();
             $user->setUsername($name);
-            $user->setPassword($name);
+            $encoder = $this->container->get('security.password_encoder');
+            $password = $encoder->encodePassword($user, $name);
+            $user->setPassword($password);
             $user->setSalt('');
             $user->setRoles(array('ROLE_USER'));
+            $user->setUsernameCanonical($name);
+            $user->setEmail($name . '@' . $name . '.com');
+            $user->setEnabled(true);
 
             $manager->persist($user);
         }
 
         $user_admin = new User();
         $user_admin->setUsername('admin');
-        $user_admin->setPassword('admin');
+        $encoder = $this->container->get('security.password_encoder');
+        $password = $encoder->encodePassword($user_admin, 'admin');
+        $user_admin->setPassword($password);
         $user_admin->setSalt('');
         $user_admin->setRoles(array('ROLE_ADMIN'));
+        $user_admin->setUsernameCanonical($name);
+        $user_admin->setEmail('admin@admin.com');
+        $user_admin->setEnabled(true);
+
         $manager->persist($user_admin);
 
         $manager->flush();
